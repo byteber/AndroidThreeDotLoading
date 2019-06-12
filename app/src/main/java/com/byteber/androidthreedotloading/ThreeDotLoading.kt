2f068e990e89836db8b8fbadc188970cc90a0ea2
byteber <admin@byteber.com>
2019-06-12 12:00:56 +0800

    Configure default width and height

--- app/src/main/java/com/byteber/androidthreedotloading/ThreeDotLoading.kt
+++ app/src/main/java/com/byteber/androidthreedotloading/ThreeDotLoading.kt
@@ -8,4 +8,9 @@ class ThreeDotLoading @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        setMeasuredDimension(heightSize * 3, heightSize)
+    }
 }
