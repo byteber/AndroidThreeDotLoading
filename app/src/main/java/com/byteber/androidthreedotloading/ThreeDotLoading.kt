667d702f3dadcae4b1216e5c1fe0c7bb6674aa5c
byteber <admin@byteber.com>
2019-06-12 15:03:55 +0800

    Apply scale animation to dot by TimeAnimator

--- app/src/main/java/com/byteber/androidthreedotloading/ThreeDotLoading.kt
+++ app/src/main/java/com/byteber/androidthreedotloading/ThreeDotLoading.kt
@@ -1,5 +1,6 @@
 package com.byteber.androidthreedotloading
 
+import android.animation.TimeAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -14,6 +15,45 @@ class ThreeDotLoading @JvmOverloads constructor(
     private val dotPaint = Paint().apply {
         color = Color.RED
     }
+    private val dotScales = floatArrayOf(0f, 0f, 0f)
+    private var animator: TimeAnimator? = null
+
+    override fun onVisibilityChanged(changedView: View?, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (visibility == View.VISIBLE) {
+            if (animator == null) {
+                createAnimator()
+            }
+            animator?.start()
+        } else {
+            animator?.cancel()
+        }
+    }
+
+    /**
+     * return scale value as flow 0 -> 1 -> 0 -> 0
+     */
+    private fun createAnimator() {
+        animator = TimeAnimator().apply {
+            setTimeListener { timeAnimator, totalTime, deltaTime ->
+                dotScales[0] = getNextScale(totalTime)
+                dotScales[1] = getNextScale(totalTime - 350)
+                dotScales[2] = getNextScale(totalTime - 700)
+                invalidate()
+            }
+        }
+    }
+
+    private fun getNextScale(totalTime: Long): Float {
+        val periodTime = totalTime % 3000
+        if (periodTime >= 2000) {
+            return 0f
+        }
+        if (periodTime <= 1000) {
+            return periodTime.toFloat() / 1000
+        }
+        return (2000 - periodTime).toFloat() / 1000
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -21,7 +61,7 @@ class ThreeDotLoading @JvmOverloads constructor(
         for (i in 0..2) {
             val dotWidth = width / 3
             val dotHeight = height
-            val dotRadius = dotWidth.toFloat() / 2
+            val dotRadius = dotWidth.toFloat() / 2 * dotScales[i]
             val dotCenterX = dotWidth * i + dotWidth.toFloat() / 2
             val dotCenterY = dotHeight.toFloat() / 2
 
